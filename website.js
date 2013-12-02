af79ff154be70d043be6d4b074ff519106b416a4
John Kershaw <wardrox@gmail.com>
2013-12-02 21:12:18 +0000

    ng therey

--- website.js
+++ website.js
@@ -2,9 +2,24 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  res.render('index.htm');
 });
+
+app.post('/status', function(request, response) {
+	// save the response
+  response.send(200);
+});
+
+app.get('/status', function(request, response) {
+	// save the response
+  response.send("This is a status");
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
