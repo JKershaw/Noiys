f09cfc66e9c0cab90a2ae306e4153841c49d63b8
John Kershaw <wardrox@gmail.com>
2013-12-28 17:19:07 +0000

    nd more favicon stuffy

--- website.js
+++ website.js
@@ -4,12 +4,12 @@ var express = require("express"),
     ROOT_DIRECTORY = __dirname,
     ASSETS_DIRECTORY = path.join(ROOT_DIRECTORY, 'public');
 
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger());
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(express.bodyParser());
 app.use(express.static(ASSETS_DIRECTORY));
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 
 require("./routes/home")(app);
 require("./routes/status")(app);
