e3ad1ce0074d4accbd4ba06db9b383f7c5b8df1a
Sausage <wardrox@gmail.com>
2014-01-10 16:53:14 +0000

    cleaning

--- public/javascript/noise-publish-status.js
+++ public/javascript/noise-publish-status.js
@@ -12,31 +12,16 @@ define(['jquery', 'noise-starred', 'noiys-vote', 'noiys-ui', 'noise-starred'], f
 		
 		noiseStarred.update_star_html(status.id);
 
-		var quote_to_hide_selector = $(wrapper + " #" + status.id + " > div.panel-body > div.panel > div.panel-body > div.panel > div.panel-body > div.panel");
-
-		if (quote_to_hide_selector) {
-			quote_to_hide_selector.after(noiysUi.generate_show_quote_html(status.id));
-			quote_to_hide_selector.hide();
-		}
-
 		$("span.timeago").timeago();
 	}
 
 	function replace_status(status, wrapper, statusIDToReplace) {
-
 
 		$(wrapper + " #" + statusIDToReplace).before(noiysUi.generate_status_html(status));
 		$(wrapper + " #" + status.id).fadeIn();
 		$(wrapper + " #" + statusIDToReplace).hide();
 		
 		noiseStarred.update_star_html(status.id);
-
-		var quote_to_hide_selector = $(wrapper + " #" + status.id + " > div.panel-body > div.panel > div.panel-body > div.panel > div.panel-body > div.panel");
-
-		if (quote_to_hide_selector) {
-			quote_to_hide_selector.after(noiysUi.generate_show_quote_html(status.id));
-			quote_to_hide_selector.hide();
-		}
 
 		$("span.timeago").timeago();
 	}
